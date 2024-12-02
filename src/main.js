a7e520aadb56eb3e6f0eb7ab5501e197e0910d0b
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2024-12-02 16:15:00 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 800
+    height: 800,
+
+    webPreferences: {
+      preload: path.join(__dirname, '../preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
   })
 
   win.loadFile('./src/index.html')
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
   createWindow()
+
+  ipcMain.on('add-product', (event, product) => {
+    console.log('producto anadido', product)
+
+    event.reply('product-added', product)
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 })
