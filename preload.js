ea1120ee37a5adddcdef82a3b3d724fdd1dc2abd
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2024-12-15 22:48:03 -0500

    Update preload.js
    conexion segura con base de datos

--- preload.js
+++ preload.js
@@ -5,8 +5,18 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('paletteAPI', {
   Products: {
     getProducts: async () => {
-      console.log('Llamando a get-products desde renderer')
+      // console.log('Llamando a get-products desde renderer')
       return await ipcRenderer.invoke('get-products')
+    }
+  },
+  Invoice: {
+    addInvoice: async (invoice) => {
+    // console.log('Llamando a db:add-invoice desde renderer con los datos:', invoice)
+      return await ipcRenderer.invoke('db:add-invoice', invoice)
+    },
+    getInvoice: async (invoiceId) => {
+    // console.log('Llamando a db:getinvoice para la factura con id:', invoiceId)
+      return await ipcRenderer.invoke('db:getinvoice', invoiceId)
     }
   }
 })
