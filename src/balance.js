f09b26c61e6b5d3bd536975b28701e7aaac9b930
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-01-06 14:37:04 -0500

    Update balance.js
    V1.0

--- src/balance.js
+++ src/balance.js
@@ -1,38 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const calendarButton = document.querySelector('.calendarButton')
+  const dateInput = document.querySelector('#start')
 
-    const base = document.getElementById('base-input')
-    const baseButton = document.getElementById('base-button')
-    const calendarButton = document.querySelector('.calendarButton')
-    const dateInput = document.querySelector('#start')
+  const balanceList = document.querySelector('.balance-list')
 
-    const balanceList = document.querySelector('.balance-list')
+  try {
+    function renderBalance (balanceData, container, selectedDate) {
+      container.innerHTML = ''
 
-    try {
+      if (!balanceData || !balanceData.desglose || balanceData.desglose.length === 0) {
+        container.innerHTML = '<p>No se encontraron facturas para el día seleccionado.</p>'
+        return
+      }
 
-        function renderBalance(balanceData, container ,selectedDate) {
-            container.innerHTML = ''; // Limpia el contenido del contenedor
-        
-            if (!balanceData || !balanceData.desglose || balanceData.desglose.length === 0) {
-                container.innerHTML = '<p>No se encontraron facturas para el día seleccionado.</p>';
-                return;
-            }
-        
-            // Crear elementos para mostrar el resumen
-            const dateDiv = document.createElement('div');
-            dateDiv.className = 'date-balance';
-            dateDiv.innerHTML = `<h2>Balance con fecha: ${selectedDate}</h2>`;
-            const totalDiv = document.createElement('div');
-            totalDiv.className = 'total-balance';
-            totalDiv.innerHTML = `<h3>Total del día: ${balanceData.total_general.toLocaleString()}</h3>`;
-            container.appendChild(dateDiv);
-            container.appendChild(totalDiv);
-            
-        
-            // Iterar sobre el desglose para mostrar cada tipo de pago
-            balanceData.desglose.forEach(item => {
-                const paymentDiv = document.createElement('div');
-                paymentDiv.className = `balance-${item.tipo_pago}`;
-                paymentDiv.innerHTML = `
+      // Crear elementos para mostrar el resumen
+      const dateDiv = document.createElement('div')
+      dateDiv.className = 'date-balance'
+      dateDiv.innerHTML = `<h2>Balance con fecha: ${selectedDate}</h2>`
+      const totalDiv = document.createElement('div')
+      totalDiv.className = 'total-balance'
+      totalDiv.innerHTML = `<h3>Total del día: ${balanceData.total_general.toLocaleString()}</h3>`
+      container.appendChild(dateDiv)
+      container.appendChild(totalDiv)
+
+      // Iterar sobre el desglose para mostrar cada tipo de pago
+      balanceData.desglose.forEach(item => {
+        const paymentDiv = document.createElement('div')
+        paymentDiv.className = `balance-${item.tipo_pago}`
+        paymentDiv.innerHTML = `
                     <h4>Tipo de pago: ${item.tipo_pago}</h4>
                     <p>Total: ${item.total.toLocaleString()}</p>
                     ${
@@ -45,43 +40,31 @@ document.addEventListener('DOMContentLoaded', () => {
                             ? `<p>Por tarjeta: ${item.tarjeta.toLocaleString()}</p>`
                             : ''
                     }
-                `;
-                container.appendChild(paymentDiv);
-            });
-        }
-        
-        
-        baseButton.addEventListener('click', async () => {
-            const baseValue = parseFloat(base.value)
-            console.log('Base:', baseValue)
-            console.log(typeof baseValue)
-        })
-
-        calendarButton.addEventListener('click', async () => {
-            try {
-              const selectedDate = dateInput.value
-              console.log('Fecha seleccionada:', selectedDate)
-        
-              if (!selectedDate) {
-                console.error('Por favor, selecciona una fecha.')
-                return
-              }
-        
-            const balance = await window.paletteAPI.Operations.filterByDay(selectedDate)
-            console.log('Facturas filtradas:', balance)
-            console.log(typeof balance)
-            renderBalance(balance, balanceList, selectedDate)
-              
-            } catch (error) {
-              console.error('Error al obtener las facturas:', error)
-            }
-          })
-
-        
-
-    } catch (error) {
-        console.error('Error al manejar la base', error)
-        throw error
+                `
+        container.appendChild(paymentDiv)
+      })
     }
 
+    calendarButton.addEventListener('click', async () => {
+      try {
+        const selectedDate = dateInput.value
+        console.log('Fecha seleccionada:', selectedDate)
+
+        if (!selectedDate) {
+          console.error('Por favor, selecciona una fecha.')
+          return
+        }
+
+        const balance = await window.paletteAPI.Operations.filterByDay(selectedDate)
+        console.log('Facturas filtradas:', balance)
+        console.log(typeof balance)
+        renderBalance(balance, balanceList, selectedDate)
+      } catch (error) {
+        console.error('Error al obtener las facturas:', error)
+      }
+    })
+  } catch (error) {
+    console.error('Error al manejar la base', error)
+    throw error
+  }
 })
